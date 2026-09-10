a2d0c55b45e92a4e659ec66831a407e63f1f3a97
Lakshmi Siva Prasad Yendakurthy <ysivap710@gmail.com>
2026-09-10 14:17:26 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# study
+A mobile-first personal Linktree-style profile with responsive desktop/mobile UI, customizable backgrounds, DP, and social links.  Hosted on **GitHub Pages**, images on **ImageKit**, and official social icons stored locally as **SVGs from Simple Icons**.
---
+++ js/config.js
@@ -0,0 +1,36 @@
+const PROFILE_CONFIG = {
+
+    name: "Y. Siva Prasad",
+
+    profileImage:
+        "./assets/images/profile.png",
+    background:
+        "",
+    socials: [
+        {
+            name: "Drive",
+            url: "https://drive.google.com/drive/folders/1xgEIk-ebaUMrNaueY9wydpoyKNxlSvPc",
+            icon: "./assets/icons/googledrive.svg"
+        },
+        {
+            name: "GitHub",
+            url: "https://github.com/siva7101984",
+            icon: "./assets/icons/github.svg"
+        },
+        {
+            name: "LinkedIn",
+            url: "https://www.linkedin.com/in/ysiva-prasad-5babab366/",
+            icon: "./assets/icons/linkedin.svg"
+        },
+        {
+            name: "Hackerrank",
+            url: "https://www.hackerrank.com/profile/ysivap710",
+            icon: "./assets/icons/hackerrank.svg"
+        },
+        {
+            name: "LeetCode",
+            url: "https://leetcode.com/u/ysivap/",
+            icon: "./assets/icons/leetcode.svg"
+        }
+    ]
+};
